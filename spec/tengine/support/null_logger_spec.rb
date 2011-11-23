8a21aeb6d1a2419735505f2990ded1ee8b9f4ae8
akima <akima@nautilus-technologies.com>
2011-11-23 21:42:25 +0900

    [Tengine::Support::Config使用時のfieldに指定した:typeが無視されている] 各specでのspec_helperへの相対パスが一つ深くなったので対応しました。

--- spec/tengine/support/null_logger_spec.rb
+++ spec/tengine/support/null_logger_spec.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Tengine::Support::NullLogger do
   context "使用可能なメソッド" do
